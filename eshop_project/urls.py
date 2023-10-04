c0f7a58091eec60b1841e36813bda156a880144c
Masoud Pirhadi <146786001+EvilSick@users.noreply.github.com>
2023-10-04 12:32:14 +0330

    urls.py

--- eshop_project/urls.py
+++ eshop_project/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chat_module.urls')),
+    path('', include('home_module.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
