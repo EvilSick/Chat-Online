9de568907dc02c395a4ba394aa39e0af6133991f
Masoud Pirhadi <146786001+EvilSick@users.noreply.github.com>
2023-10-04 12:30:54 +0330

    urls.py

--- eshop_project/urls.py
+++ eshop_project/urls.py
@@ -21,16 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin-panel/', include('admin_panel.urls')),
-    path('contact-us/', include('contact_module.urls')),
-    path('articles/', include('article_module.urls')),
-    path('products/', include('product_module.urls')),
-    path('user/', include('user_panel_module.urls')),
-    path('user/ticket/', include('ticket_module.urls')),
-    path('order/', include('order_module.urls')),
     path('chat/', include('chat_module.urls')),
-    path('', include('home_module.urls')),
-    path('', include('account_module.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
